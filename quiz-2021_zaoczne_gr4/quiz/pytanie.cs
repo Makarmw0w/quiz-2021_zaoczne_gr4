4239c70339600e9f2136cad74baadfca35f0fdec
Makar <110151@student.san.edu.pl>
2021-10-31 16:39:35 +0100

    save1

---
+++ quiz-2021_zaoczne_gr4/quiz/pytanie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz
+{
+    class Pytanie
+    { 
+
+        public int ID { get; set; }
+        public int Kategoria { get; set; }
+        public string Tresc { get; set; }
+        public string Odp1 { get; set; }
+        public string Odp2 { get; set; }
+        public string Odp3 { get; set; }
+        public string Odp4 { get; set; }
+
+    }
+}
